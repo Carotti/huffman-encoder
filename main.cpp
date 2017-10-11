62e2a84c1cb754c292f43d81f88614980f899736
Thomas Carotti <thomas@carotti.co.uk>
2017-10-11 19:19:44 +0100

    Reading from a file

--- main.cpp
+++ main.cpp
@@ -7,12 +7,22 @@
 
 #include "huffman.hpp"
 
-void get_stdin(std::string& str)
+const static std::string decode_arg = "--decode";
+const static std::string encode_arg = "--encode";
+const static std::string verbose_arg = "--verbose";
+const static std::string file_arg = "--file";
+
+const static char decode_short_arg = 'd';
+const static char encode_short_arg = 'e';
+const static char verbose_short_arg = 'v';
+const static char file_short_arg = 'f';
+
+void get_input(std::string& str, std::istream& from)
 {
     // Don't skip whitespace
-    std::cin >> std::noskipws;
+    from >> std::noskipws;
 
-    str.assign((std::istreambuf_iterator<char>(std::cin)),
+    str.assign((std::istreambuf_iterator<char>(from)),
                 std::istreambuf_iterator<char>());
 }
 
@@ -21,14 +31,20 @@ int main(int argc, char* argv[])
     bool decode = false;
     bool encode = false;
 
+    bool from_file = false;
+    std::string file_name;
+
     int verbosity = 0;
 
     for (int i = 1; i < argc; i++) {
         std::string arg(argv[i]);
         std::map<char, int> short_args;
 
+        bool valid_arg = false;
+
         // Handle single letter arguments
         if (arg[0] == '-' && arg[1] != '-') {
+            valid_arg = true;
             for (int j = 1; j < arg.size(); j++) {
                 if (short_args.find(arg[j]) == short_args.end()) {
                     short_args[arg[j]] = 1;
@@ -38,19 +54,40 @@ int main(int argc, char* argv[])
             }
         }
 
-        bool valid_arg = true;
-
-        if (arg == "--decode" || short_args.find('d') != short_args.end()) {
+        // --decode or -d
+        if (arg == decode_arg || short_args.find(decode_short_arg) != short_args.end()) {
             decode = true;
-            short_args.erase('d');
-        } else if (arg == "--encode" || short_args.find('e') != short_args.end()) {
+            short_args.erase(decode_short_arg);
+            valid_arg = true;
+        }
+
+        // --encode or -e
+        if (arg == encode_arg || short_args.find(encode_short_arg) != short_args.end()) {
             encode = true;
-            short_args.erase('e');
-        } else if (arg == "--verbose" || short_args.find('v') != short_args.end()) {
-            verbosity++;
-            short_args.erase('v');
-        } else {
-            valid_arg = false;
+            short_args.erase(encode_short_arg);
+            valid_arg = true;
+        }
+
+        // --verbose or -v
+        if (arg == verbose_arg || short_args.find(verbose_short_arg) != short_args.end()) {
+            if (arg == verbose_arg) {
+                verbosity++;
+            } else {
+                verbosity += short_args[verbose_short_arg];
+            }
+
+            short_args.erase(verbose_short_arg);
+            valid_arg = true;
+        }
+
+        // --file or -f
+        if (arg == file_arg || short_args.find(file_short_arg) != short_args.end()) {
+            from_file = true;
+
+            // file name is the next argument
+            file_name = std::string(argv[++i]);
+            short_args.erase(file_short_arg);
+            valid_arg = true;
         }
 
         if (!short_args.empty() || !valid_arg) {
@@ -64,18 +101,29 @@ int main(int argc, char* argv[])
     }
 
     std::string input;
-    get_stdin(input);
+    std::string output;
+
+    // - indicates to read from stdin
+    if (!from_file || file_name == "-") {
+        get_input(input, std::cin);
+    } else {
+        // Read from file
+        std::ifstream file;
+        file.open(file_name.c_str());
+
+        get_input(input, file);
+
+        file.close();
+    }
 
     if (encode) {
-
+        huffman::encode(input, output);
     } else if (decode) {
-
+        huffman::decode(input, output);
     } else {
         std::cerr << "Please specify either --encode, -e or --decode, -d" << std::endl;
         exit(EXIT_FAILURE);
     }
-
-    std::string output;
 
     std::cout << output;
 }
